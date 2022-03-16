cc42dc0308a555b4d457efb965e3d28c14c566b6
estrattonbailey <ericstrattonbailey@gmail.com>
2022-03-16 15:31:20 -0700

    feat: update types, v3

--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
-export default function wait (
+/**
+ * @see https://levelup.gitconnected.com/crazy-powerful-typescript-tuple-types-9b121e0a690c
+ */
+type UnwrapPromise<T> = T extends Promise<infer U> ? U : T;
+type UnwrapPromises<T extends [...any[]]> =
+    T extends [infer Head, ...infer Tail]
+        ? [UnwrapPromise<Head>, ...UnwrapPromises<Tail>]
+        : [];
+type RemapPromises<T extends [...any[]]> = Promise<UnwrapPromises<T>>;
+
+export default function wait<T extends [...any[]]>(
   delay: number,
-  fns: any = []
-) {
+  fns: [...T]
+): RemapPromises<T> {
   return Promise
     .all(
       [].concat(fns).concat(
         new Promise(r => setTimeout(r, delay))
       )
     )
-    .then(arr => arr.slice(0, arr.length - 1))
+    .then(arr => arr.slice(0, arr.length - 1)) as RemapPromises<T>
 }
--- test.ts
+++ test.ts
@@ -1,21 +1,33 @@
-import test from 'ava';
+import { test } from 'uvu'
+import * as assert from 'uvu/assert'
+
 import wait from './index';
 
 test('returns array', async t => {
-  const res = await wait(0, 'foo');
-  console.log(res)
-  t.truthy(Array.isArray(res))
+  const res = await wait(0, ['foo']);
+  assert.ok(Array.isArray(res))
 })
 
 test('returns values in order', async t => {
   const res = await wait(0, [ 'foo', 'bar' ]);
-  t.is(res[0], 'foo')
-  t.is(res[1], 'bar')
+  assert.equal(res[0], 'foo')
+  assert.equal(res[1], 'bar')
 })
 
 test('resolves promises', async t => {
   const res = await wait(0, [
     Promise.resolve('foo')
   ]);
-  t.is(res[0], 'foo')
+  assert.equal(res[0], 'foo')
 })
+
+test('types resolve', async t => {
+  const [str, num] = await wait<[Promise<string>, Promise<number>]>(0, [
+    Promise.resolve('foo'),
+    Promise.resolve(1),
+  ]);
+  assert.equal(str, 'foo')
+  assert.equal(num, 1)
+})
+
+test.run()
